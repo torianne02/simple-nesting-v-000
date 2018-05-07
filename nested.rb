4c9b036182b78c6ce910af120199f8ea3092d7ae
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-07 21:06:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -73,6 +73,8 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto] = {}
+	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
 	# add the following information to the top level of programmer_hash
 	# :yukihiro_matsumoto => {
