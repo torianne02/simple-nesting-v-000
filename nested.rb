5b6d074b146e11e344693c8b0370f50cec48878d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-07 21:24:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages].join
+	programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
